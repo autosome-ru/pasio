181481caa5d130fcb1ec4df768ecd208c11867a3
Lando Andrey <dronte@autosome.ru>
2017-05-31 18:47:22 +0300

    Added average counts column to resulting bedgraph.

--- src/pasio.py
+++ src/pasio.py
@@ -202,8 +202,8 @@ def split_bedgraph(in_filename, out_filename, scorer_factory,
             scorer = scorer_factory(counts, splits+[len(counts)])
             logger.info('Starting output of chrom %s' % (chrom))
             for i, (start, stop) in enumerate(zip(splits, splits[1:])):
-                outfile.write('%s\t%d\t%d\t%f\n' % (chrom, start, stop, scorer(i, i+1)))
-            outfile.write('%s\t%d\t%d\t%f\n' % (chrom, splits[-1], len(counts), scorer(len(splits)-1)))
+                outfile.write('%s\t%d\t%d\t%f\t%f\n' % (chrom, start, stop, counts[start:stop].mean(), scorer(i, i+1)))
+            outfile.write('%s\t%d\t%d\t%f\t%f\n' % (chrom, splits[-1], len(counts), counts[start:stop].mean(), scorer(len(splits)-1)))
 
 
 if __name__ == '__main__':
