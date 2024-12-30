3000de83e45bdf84e3aaabd3262679ca026ca087
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2024-12-30 07:33:55 +0300

    large counts can be provided in scientific notation, in that case warn about them and convert to an integer through a float

--- src/pasio/dto/intervals.py
+++ src/pasio/dto/intervals.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from ..utils.gzip_utils import open_for_read
+from ..logging import logger
 
 class ScoredInterval( namedtuple('ScoredInterval', ['start', 'stop', 'mean_count', 'log_marginal_likelyhood']) ):
     @property
@@ -13,10 +14,14 @@ class BedgraphInterval(namedtuple('BedgraphInterval', ['chrom', 'start', 'stop',
 
     @classmethod
     def from_string(cls, line):
-        chrom, start, stop, count = line.split()[0:4]
+        chrom, start, stop, count_str = line.split()[0:4]
         start = int(start)
         stop = int(stop)
-        count = int(count)
+        try:
+            count = int(count_str)
+        except:
+            count = int(float(count_str))
+            logger.warn(f"Pasio cannot be used with floating point counts. `{count_str}` was automatically converted to an integer `{count}` as an approximation. Make sure this value was designed to actually be an integer count.")
         return cls(chrom, start, stop, count)
 
     @classmethod
